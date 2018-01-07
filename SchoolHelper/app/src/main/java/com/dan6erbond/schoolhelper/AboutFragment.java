5ab4db7f9797e7c04f2c38fe0f2b627481916835
Dan6erbond <32235074+Dan6erbond@users.noreply.github.com>
2018-01-07 18:00:17 +0100

    changed folder structure, all app data is in /SchoolHelper and added a folder for the RAW Logo

---
+++ SchoolHelper/app/src/main/java/com/dan6erbond/schoolhelper/AboutFragment.java
@@ -0,0 +1,45 @@
+package com.dan6erbond.schoolhelper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        final Button websiteButton = view.findViewById(R.id.button_website);
+        websiteButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://dan6erbond.github.io"));
+                        startActivity(browserIntent);
+                    }
+                }
+        );
+
+        final Button updatesButton = view.findViewById(R.id.button_check_updates);
+        updatesButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Dan6erbond/SchoolHelper/releases"));
+                        startActivity(browserIntent);
+                    }
+                }
+        );
+
+        return view;
+    }
+}
